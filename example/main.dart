a6bef4e7e4d99276d46f5d2834073835fa8d9291
Jesús Hernández Barrios <jhernandezb96@gmail.com>
2026-07-06 12:04:57 -0600

    [example] Add route basic example

--- example/main.dart
+++ example/main.dart
@@ -1,5 +1,18 @@
 import 'package:winter/winter.dart';
 
-void main() => Winter.start(
-  router: ServeRouter((request) => ResponseEntity.ok(body: 'Hello world!!!')),
-);
+void main() async {
+  await Winter.start(
+    config: ServerConfig(port: 8080),
+    router: WinterRouter(
+      config: RouterConfig(onLoadedRoutes: DefaultOnLoadedRoutes.log()),
+      routes: [
+        Route.get(
+          path: '/hello-wold',
+          handler: (request) {
+            return ResponseEntity.ok(body: 'Hello World');
+          },
+        ),
+      ],
+    ),
+  );
+}
